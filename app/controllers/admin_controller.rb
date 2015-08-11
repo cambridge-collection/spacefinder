b0fe07828f0e3390f052737d356c1c5db4344046
Dan W <git@iamdanw.com>
2015-08-11 18:01:03 +0100

    Fixed incorrect Tip count on admin dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
   def index
     @spaces = Space.order('name').pluck(:id, :name)
-    @tips_review_count = Tip.count
+    @tips_review_count = Tip.count(:response)
   end
 end
