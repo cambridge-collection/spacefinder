2d8c096ef585ec6b24fad10597865f8dd3081227
Dan W <git@iamdanw.com>
2015-08-17 11:41:27 +0100

    Edit Space picker now grouped by building name

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,7 @@
 class AdminController < ApplicationController
   def index
-    @spaces = Space.order('name').pluck(:id, :name)
+    spaces = Space.order('name').pluck(:id, :name, :library_id)
+    @space_groups = spaces.group_by{|space| space[2].nil? ? "No Building" : Library.find(space[2]).name }
     @tips_review_count = Tip.count(:response)
     @disable_back_button = true
   end
