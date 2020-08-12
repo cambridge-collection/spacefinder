8380116355d496eb9619911172373075981a9fd8
Tuan Pham <tp425@cam.ac.uk>
2020-08-12 11:22:36 +0100

    Working version with many warnings.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,10 +3,12 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path.
+# Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w( admin.css )
+Rails.application.config.assets.precompile += %w( home.css )
+Rails.application.config.assets.precompile += %w( admin.js )
+Rails.application.config.assets.precompile += %w( home.js )
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'admin', to: 'admin#index'
+  get 'admin/login', to: 'admin#login'
+  get 'admin/review', to: 'tips#review'
+
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_scope :user do
+    delete "/users/sign_out" => "devise/sessions#destroy"
+  end
+  resource :user, :defaults => { :format => 'json' }
+
+  get '/spaces/filters.json', to: 'spaces#filters', format: 'json'
+
+  resources :spaces do
+    resources :tips, :except => [:create]
+    resources :tips, :only => [:create], :defaults => { :format => 'json' }
+    post '/tags', action: :add_tag, format: 'json', on: :member
+  end
+
+  root 'home#index'
 end
