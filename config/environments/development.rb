443595088f96f58ad046d826413f36b46332b99e
Tuan Pham <tp425@cam.ac.uk>
2020-09-30 21:35:09 +0100

    Passed in the BASE_URL instead of hardcoded value for the javascript domain.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
@@ -60,4 +60,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  ActiveRecord::Base.verbose_query_logs = false
+
 end
