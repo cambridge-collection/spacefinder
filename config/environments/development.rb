01f7424b3ef68b813a10df7cd427ba011eb9fffb
Hal Blackburn <hwtb2@cam.ac.uk>
2022-02-04 12:33:16 +0000

    feat: allow disabling config.file_watcher in development
    When building the Docker image the EventedFileUpdateChecker blows up
    because inotify isn't available.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker unless ENV['DISABLE_FILE_WATCHER']
 
   ActiveSupport::Deprecation.silenced = true
 end
